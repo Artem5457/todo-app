866e344041d6067cbffe8e9740c256cdbef7f5c7
Artem Bolshak <fcdnipro20152705@gmail.com>
2021-12-08 13:13:27 +0200

    Updates

--- static/js/main.0e9da3d5.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonptodo-app"]=this["webpackJsonptodo-app"]||[]).push([[0],{9:function(e,t,c){"use strict";c.r(t);var n=c(1),o=c.n(n),r=c(3),s=c.n(r),d=c(0);var p=function(){return Object(d.jsx)("div",{className:"App",children:Object(d.jsx)("h1",{children:"Hello, world!"})})};s.a.render(Object(d.jsx)(o.a.StrictMode,{children:Object(d.jsx)(p,{})}),document.getElementById("root"))}},[[9,1,2]]]);
-//# sourceMappingURL=main.0e9da3d5.chunk.js.map
